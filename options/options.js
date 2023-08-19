342a65bbe82dca62c0215b1270d1ee2feb29a9c9
Crowdbotics <team@crowdbotics.com>
2023-08-19 13:52:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "supayroll-42948",
+  url: "https://supayroll-42948.botics.co",
+  api: "https://supayroll-42948.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
